44de5fe10b0432ff162583a9ec1ce083644f2a48
Otavio Henrique <otaviohenriquevieiradasilva@gmail.com>
2026-08-18 19:24:35 -0300

    add codes for implemention web in spring web

--- src/main/java/com/tads/First_api/FirstApiApplication.java
+++ src/main/java/com/tads/First_api/FirstApiApplication.java
@@ -2,12 +2,20 @@ package com.tads.First_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstApiApplication.class, args);
+		SpringApplication.run(FirstApiApplication.class, args);	
+	}
+
+	@GetMapping("/")
+	public String helloWorld() {
+		return "hello world!";
 	}
 
 }
